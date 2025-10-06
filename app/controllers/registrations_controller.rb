c533bfea260fe66774f32051e2d345f0ecf8ded4
Muhammad Asim <mughalma096@gmail.com>
2025-10-06 23:04:45 +0500

    intial commit

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,24 @@
+class RegistrationsController < ApplicationController
+  allow_unauthenticated_access
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      start_new_session_for @user
+      redirect_to root_path, notice: "Signed up successfully!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email_address, :password, :password_confirmation)
+  end
+end
