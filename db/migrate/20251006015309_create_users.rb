4fd790a49e11d07005a5c8ee6bf7e4e88fefe7bf
Muhammad Asim <mughalma096@gmail.com>
2025-10-07 06:56:07 +0500

    Updated forms design

--- db/migrate/20251006015309_create_users.rb
+++ db/migrate/20251006015309_create_users.rb
@@ -1,6 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
       t.string :email_address, null: false
       t.string :password_digest, null: false
 
