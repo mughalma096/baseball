9711ac9e72d504c57dac6d9b0b8f28762de77e84
Muhammad Asim <mughalma096@gmail.com>
2025-10-24 23:13:05 +0500

    updated the note model

--- db/migrate/20251024181215_create_notes.rb
+++ db/migrate/20251024181215_create_notes.rb
@@ -1,6 +1,10 @@
 class CreateNotes < ActiveRecord::Migration[8.0]
   def change
     create_table :notes do |t|
+      t.text :body
+      t.integer :status
+      t.references :notable, polymorphic: true, null: false
+
       t.timestamps
     end
   end
